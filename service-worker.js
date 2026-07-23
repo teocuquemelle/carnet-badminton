64e8b2f0a51a6b83814f0971bf7fd9b6b9f7eeb0
teocuquemelle <pc7rdqp57j@privaterelay.appleid.com>
2026-07-23 18:38:28 +0200

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -13,7 +13,7 @@
  * l'application pour forcer les appareils à récupérer la
  * dernière version au prochain passage en ligne.
  */
-const CACHE_VERSION = "badminton-v7";
+const CACHE_VERSION = "badminton-v8";
 const APP_SHELL = [
   "./index.html",
   "./manifest.json",
